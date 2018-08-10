9b9eb9842c580f2c0d3fafcb743501ced9a8c9d9
Pradeep Kumar <PKumar1@mindtree_odc01.com>
2018-08-10 15:53:58 +0530

    Added new method

--- src/main/java/com/cruds/demo/mvndemo/App.java
+++ src/main/java/com/cruds/demo/mvndemo/App.java
@@ -4,13 +4,20 @@ package com.cruds.demo.mvndemo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
-    public String getGreeting(){
-    	return "Hello from Maven";
+
+    public String getGreeting() {
+        return "Hello from Maven";
+    }
+
+    public boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
